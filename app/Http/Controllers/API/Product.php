a44a40e357888520afd30286eaf0e49c524b0442
TAjELDEEN HASSAN <41596304+taghassan54@users.noreply.github.com>
2024-08-03 17:44:51 +0400

    Update Product.php

--- app/Http/Controllers/API/Product.php
+++ app/Http/Controllers/API/Product.php
@@ -394,7 +394,7 @@ class Product extends Controller
             }
 
             $list = new ProductCollection(ProductModel::select('*')
-            ->orderBy('created_at', 'desc')->paginate());
+            ->orderBy('created_at', 'desc')->cursorPaginate(ProductModel::count()));
 
             return response()->json($this->generate_response(
                 array(
