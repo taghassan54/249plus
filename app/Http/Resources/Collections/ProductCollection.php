cdf0465bd8d5eefd383e685659196bd4d0f3592e
TAjELDEEN HASSAN <41596304+taghassan54@users.noreply.github.com>
2024-08-03 20:07:15 +0400

    Update ProductCollection.php

--- app/Http/Resources/Collections/ProductCollection.php
+++ app/Http/Resources/Collections/ProductCollection.php
@@ -19,7 +19,7 @@ class ProductCollection extends ResourceCollection
             'data' => ProductResource::collection($this->collection),
             'links' => [
                 'has_more_items' => $this->hasMorePages(),
-                'current_page' => $this->currentPage(),
+//                'current_page' => $this->currentPage(),
                 'next_page' => $this->nextPageUrl(),
                 'total_records' => $this->total()
             ]
