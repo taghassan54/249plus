63c976921b6476b953be4ecb9f9998c9707802d8
TAjELDEEN HASSAN <41596304+taghassan54@users.noreply.github.com>
2024-08-05 12:08:28 +0400

    remove changes

--- app/Http/Resources/ProductIngredientResource.php
+++ app/Http/Resources/ProductIngredientResource.php
@@ -17,35 +17,35 @@ class ProductIngredientResource extends JsonResource
         $ingredient_product = collect();
         $low_stock = 0;
         if(!empty($this->ingredient_product)){
-            // $ingredient_product = new ProductResource($this->ingredient_product);
+            $ingredient_product = new ProductResource($this->ingredient_product);
 
 
             //TODO Test This
-            $ingredient=collect([
-                'slack' =>$this->ingredient_product->slack,
-                'product_code' =>$this->ingredient_product->product_code,
-                'name' =>$this->ingredient_product->name,
-                'description' =>$this->ingredient_product->description,
-                'quantity' =>$this->ingredient_product->quantity,
-                'alert_quantity' =>$this->ingredient_product->alert_quantity,
-                'purchase_amount_excluding_tax' =>$this->ingredient_product->purchase_amount_excluding_tax,
-                'sale_amount_excluding_tax' =>$this->ingredient_product->sale_amount_excluding_tax,
-                'sale_amount_including_tax' =>$this->ingredient_product->sale_amount_including_tax,
-                'images' => ProductImageResource::collection($this->ingredient_product->product_images),
-                'is_ingredient' =>$this->ingredient_product->is_ingredient,
-                'is_ingredient_price' =>$this->ingredient_product->is_ingredient_price,
-                'is_addon_product' => $this->ingredient_product->is_addon_product,
-                'created_at_label' =>$this->ingredient_product->parseDate($this->created_at),
-                'updated_at_label' =>$this->ingredient_product->parseDate($this->updated_at),
-            ]);
-dd($ingredient->quantity);
-            // $low_stock = ($ingredient_product->quantity<=$ingredient_product->alert_quantity)?1:0;
-            $low_stock = ($ingredient->quantity<=$ingredient->alert_quantity)?1:0;
+            // $ingredient=collect([
+            //     'slack' =>$this->ingredient_product->slack,
+            //     'product_code' =>$this->ingredient_product->product_code,
+            //     'name' =>$this->ingredient_product->name,
+            //     'description' =>$this->ingredient_product->description,
+            //     'quantity' =>$this->ingredient_product->quantity,
+            //     'alert_quantity' =>$this->ingredient_product->alert_quantity,
+            //     'purchase_amount_excluding_tax' =>$this->ingredient_product->purchase_amount_excluding_tax,
+            //     'sale_amount_excluding_tax' =>$this->ingredient_product->sale_amount_excluding_tax,
+            //     'sale_amount_including_tax' =>$this->ingredient_product->sale_amount_including_tax,
+            //     'images' => ProductImageResource::collection($this->ingredient_product->product_images),
+            //     'is_ingredient' =>$this->ingredient_product->is_ingredient,
+            //     'is_ingredient_price' =>$this->ingredient_product->is_ingredient_price,
+            //     'is_addon_product' => $this->ingredient_product->is_addon_product,
+            //     'created_at_label' =>$this->ingredient_product->parseDate($this->created_at),
+            //     'updated_at_label' =>$this->ingredient_product->parseDate($this->updated_at),
+            // ]);
+
+            $low_stock = ($ingredient_product->quantity<=$ingredient_product->alert_quantity)?1:0;
+            // $low_stock = ($ingredient->quantity<=$ingredient->alert_quantity)?1:0;
         }
 
         return [
             'slack' => $this->slack,
-            'ingredient_product' => $ingredient,
+            'ingredient_product' => $ingredient_product,
             'quantity' => $this->quantity,
             'low_stock' => $low_stock,
             'measurement_unit' => new MeasurementUnitResource($this->measurement_unit),
--- app/Http/Resources/ProductVariantResource.php
+++ app/Http/Resources/ProductVariantResource.php
@@ -15,25 +15,25 @@ class ProductVariantResource extends JsonResource
    
     public function toArray($request)
     {
-        // $product = (new ProductResource($this->product_variant))->block_recurring_variants(true);
+        $product = (new ProductResource($this->product_variant))->block_recurring_variants(true);
        
-        $product=[
-            'slack' =>$this->product_variant->slack,
-            'product_code' =>$this->product_variant->product_code,
-            'name' =>$this->product_variant->name,
-            'description' =>$this->product_variant->description,
-            'quantity' =>$this->product_variant->quantity,
-            'alert_quantity' =>$this->product_variant->alert_quantity,
-            'purchase_amount_excluding_tax' =>$this->product_variant->purchase_amount_excluding_tax,
-            'sale_amount_excluding_tax' =>$this->product_variant->sale_amount_excluding_tax,
-            'sale_amount_including_tax' =>$this->product_variant->sale_amount_including_tax,
-            'images' => ProductImageResource::collection($this->product_variant->product_images),
-            'is_ingredient' =>$this->product_variant->is_ingredient,
-            'is_ingredient_price' =>$this->product_variant->is_ingredient_price,
-            'is_addon_product' => $this->product_variant->is_addon_product,
-            'created_at_label' =>$this->product_variant->parseDate($this->product_variant->created_at),
-            'updated_at_label' =>$this->product_variant->parseDate($this->product_variant->updated_at),
-        ];
+        // $product=[
+        //     'slack' =>$this->product_variant->slack,
+        //     'product_code' =>$this->product_variant->product_code,
+        //     'name' =>$this->product_variant->name,
+        //     'description' =>$this->product_variant->description,
+        //     'quantity' =>$this->product_variant->quantity,
+        //     'alert_quantity' =>$this->product_variant->alert_quantity,
+        //     'purchase_amount_excluding_tax' =>$this->product_variant->purchase_amount_excluding_tax,
+        //     'sale_amount_excluding_tax' =>$this->product_variant->sale_amount_excluding_tax,
+        //     'sale_amount_including_tax' =>$this->product_variant->sale_amount_including_tax,
+        //     'images' => ProductImageResource::collection($this->product_variant->product_images),
+        //     'is_ingredient' =>$this->product_variant->is_ingredient,
+        //     'is_ingredient_price' =>$this->product_variant->is_ingredient_price,
+        //     'is_addon_product' => $this->product_variant->is_addon_product,
+        //     'created_at_label' =>$this->product_variant->parseDate($this->product_variant->created_at),
+        //     'updated_at_label' =>$this->product_variant->parseDate($this->product_variant->updated_at),
+        // ];
        
         $variant_option = $this->variant_option;
 
