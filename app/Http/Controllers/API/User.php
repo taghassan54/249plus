2437e3c4e3b05e7f75ebf7c74f420826680c16e6
TAjELDEEN HASSAN <41596304+taghassan54@users.noreply.github.com>
2024-08-10 15:20:22 +0400

    Update User.php

--- app/Http/Controllers/API/User.php
+++ app/Http/Controllers/API/User.php
@@ -91,7 +91,7 @@ class User extends Controller
 
                         $user['id'] = $user_id;
 
-                        return $user;
+                        return $user_detail;
                         $user_stores = $this->get_available_stores($request, $user_id);
 
                         $user['stores'] = $user_stores;
